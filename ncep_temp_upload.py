6ce9f03b336ef21a29d987f72d3509a6985861ff
ptrierweiler <ptrierweiler@gmail.com>
2018-01-21 01:07:13 +0000

    clean code and and rescale to .25

--- ncep_temp_upload.py
+++ ncep_temp_upload.py
@@ -76,18 +76,17 @@ for i in range(1,max_band+1):
     cur.execute("select count(*) from pg_catalog.pg_tables where schemaname = 'ncep_temp' and \
     tablename = '{tab}'".format(tab=table))
     tab_out = cur.fetchall()[0][0]    
-    if tab_out == 0:    
+    if tab_out == 0:
         print("Extracting " + name)
-        os.system('gdal_translate -b  {b} NETCDF:"air.sig995.{yr}.nc":air {out}.tif' .format(yr=str(year), b=i, out=name))
+        os.system('gdal_translate -b {b} NETCDF:"air.sig995.{yr}.nc":air {out}.tif'.format(yr=str(year), b=i, out=name))
         print("Splitting " + name)
         os.system('gdal_translate -srcwin 0 0 72 73 -a_ullr 0 90 180 -90 {out}.tif {out}_east.tif'.format(out=name))
         os.system('gdal_translate -srcwin 72 0 72 73 -a_ullr -180 90 0 -90 {out}.tif {out}_west.tif'.format(out=name))
         print("Merging " + name.replace('.tif','_fix.tif'))
         os.system('gdal_merge.py -o {out}_fix.tif {out}_east.tif {out}_west.tif'.format(out=name))
-        db_date = datetime.strptime(tif_name.split('_')[2],'%Y%j').strftime('%Y%m%d')
         print(db_date, p)
         os.system("gdalwarp -t_srs WGS84 {out}_fix.tif {out}_wgs84.tif".format(out=name))
-        os.system("raster2pgsql -I   {out}_wgs84.tif -d ncep_temp.{tab} | psql {db}".format(out=name, tab=table, db=dbname))
+        os.system("raster2pgsql -I {out}_wgs84.tif -d ncep_temp.{tab} | psql {db}".format(out=name, tab=table, db=dbname))
         os.remove("{out}_east.tif".format(out=name))
         os.remove("{out}_west.tif".format(out=name))
         os.remove("{out}.tif".format(out=name))
@@ -95,7 +94,7 @@ for i in range(1,max_band+1):
         os.remove("{out}_fix.tif".format(out=name))
         print("{out}.tif".format(out=name))
         # resample down .5
-        cur.execute("update ncep_temp.{tab} set rast=ST_Rescale(rast, .5, -.5)".format(tab=table))
+        cur.execute("update ncep_temp.{tab} set rast=ST_Rescale(rast, .25, -.25)".format(tab=table))
     else:
         print(table + " exists in the db")
    
